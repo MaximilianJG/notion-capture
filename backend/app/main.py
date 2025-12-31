a173d3dcb33cd7d2116df72d49c8b1aebf1b6145
MaximilianJG <maximilian@glasmacher.com>
2025-12-31 17:53:32 +0100

    Make SSL workarounds development-only

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,36 +14,40 @@ import os
 import ssl
 import warnings
 
-# Fix SSL certificate issues on macOS - disable verification for development
-ssl._create_default_https_context = ssl._create_unverified_context
-os.environ['PYTHONHTTPSVERIFY'] = '0'
+# Only apply SSL workarounds in development (not production)
+IS_PRODUCTION = os.getenv("RAILWAY_ENVIRONMENT") or os.getenv("PRODUCTION")
 
-# Suppress SSL warnings in development
-import urllib3
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-warnings.filterwarnings('ignore', message='Unverified HTTPS request')
+if not IS_PRODUCTION:
+    # Fix SSL certificate issues on macOS - disable verification for development
+    ssl._create_default_https_context = ssl._create_unverified_context
+    os.environ['PYTHONHTTPSVERIFY'] = '0'
 
-# Also try certifi if available
+    # Suppress SSL warnings in development
+    import urllib3
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    warnings.filterwarnings('ignore', message='Unverified HTTPS request')
+
+    # Monkey-patch httpx to disable SSL verification (for development)
+    import httpx
+    _original_client_init = httpx.Client.__init__
+    def _patched_client_init(self, *args, **kwargs):
+        kwargs['verify'] = False
+        _original_client_init(self, *args, **kwargs)
+    httpx.Client.__init__ = _patched_client_init
+
+    _original_async_client_init = httpx.AsyncClient.__init__
+    def _patched_async_client_init(self, *args, **kwargs):
+        kwargs['verify'] = False
+        _original_async_client_init(self, *args, **kwargs)
+    httpx.AsyncClient.__init__ = _patched_async_client_init
+
+# Use certifi for proper SSL certs in production
 try:
     import certifi
     os.environ['SSL_CERT_FILE'] = certifi.where()
     os.environ['REQUESTS_CA_BUNDLE'] = certifi.where()
 except ImportError:
     pass
-
-# Monkey-patch httpx to disable SSL verification (for development)
-import httpx
-_original_client_init = httpx.Client.__init__
-def _patched_client_init(self, *args, **kwargs):
-    kwargs['verify'] = False
-    _original_client_init(self, *args, **kwargs)
-httpx.Client.__init__ = _patched_client_init
-
-_original_async_client_init = httpx.AsyncClient.__init__
-def _patched_async_client_init(self, *args, **kwargs):
-    kwargs['verify'] = False
-    _original_async_client_init(self, *args, **kwargs)
-httpx.AsyncClient.__init__ = _patched_async_client_init
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
